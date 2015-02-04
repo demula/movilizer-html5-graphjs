e99089047b54d3ff5d053e2b49675fcfb712a322
Jesús de Mula Cano <jesus.demula@movilizer.com>
2015-02-04 11:20:32 +0100

    add graphjs lib

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,235 @@
-var APP = (function($) {
+var APP = (function($, Chart) {
     "use strict";
-    var init = function() {
-        $("#hello").html("Hello world!");
+    var myNewChart;
+    var maxHeight;
+    var maxWidth;
+
+    var config = {
+        paddingHeight: 0.5,
+        paddingWidth: 0.5
     };
-    return init;
-})(jQuery);
+
+    function getPreferedHeight(percentOfAvailableScreen) {
+        return maxHeight * percentOfAvailableScreen;
+    }
+
+    function getPreferedWidth(percentOfAvailableScreen) {
+        return maxWidth * percentOfAvailableScreen;
+    }
+
+    var app = {
+        init: function(height, width) {
+            maxHeight = height;
+            maxWidth = width;
+            // Set defaults
+            Chart.defaults.global = {
+                // Boolean - Whether to animate the chart
+                animation: true,
+
+                // Number - Number of animation steps
+                animationSteps: 60,
+
+                // String - Animation easing effect
+                animationEasing: "easeOutQuart",
+
+                // Boolean - If we should show the scale at all
+                showScale: true,
+
+                // Boolean - If we want to override with a hard coded scale
+                scaleOverride: false,
+
+                // ** Required if scaleOverride is true **
+                // Number - The number of steps in a hard coded scale
+                scaleSteps: null,
+                // Number - The value jump in the hard coded scale
+                scaleStepWidth: null,
+                // Number - The scale starting value
+                scaleStartValue: null,
+
+                // String - Colour of the scale line
+                scaleLineColor: "rgba(0,0,0,.1)",
+
+                // Number - Pixel width of the scale line
+                scaleLineWidth: 1,
+
+                // Boolean - Whether to show labels on the scale
+                scaleShowLabels: true,
+
+                // Interpolated JS string - can access value
+                scaleLabel: "<%=value%>",
+
+                // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
+                scaleIntegersOnly: true,
+
+                // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+                scaleBeginAtZero: false,
+
+                // String - Scale label font declaration for the scale label
+                scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+                // Number - Scale label font size in pixels
+                scaleFontSize: 12,
+
+                // String - Scale label font weight style
+                scaleFontStyle: "normal",
+
+                // String - Scale label font colour
+                scaleFontColor: "#666",
+
+                // Boolean - whether or not the chart should be responsive and resize when the browser does.
+                responsive: true,
+
+                // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+                maintainAspectRatio: true,
+
+                // Boolean - Determines whether to draw tooltips on the canvas or not
+                showTooltips: true,
+
+                // Function - Determines whether to execute the customTooltips function instead of drawing the built in tooltips (See [Advanced - External Tooltips](#advanced-usage-custom-tooltips))
+                customTooltips: false,
+
+                // Array - Array of string names to attach tooltip events
+                tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+
+                // String - Tooltip background colour
+                tooltipFillColor: "rgba(0,0,0,0.8)",
+
+                // String - Tooltip label font declaration for the scale label
+                tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+                // Number - Tooltip label font size in pixels
+                tooltipFontSize: 14,
+
+                // String - Tooltip font weight style
+                tooltipFontStyle: "normal",
+
+                // String - Tooltip label font colour
+                tooltipFontColor: "#fff",
+
+                // String - Tooltip title font declaration for the scale label
+                tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+                // Number - Tooltip title font size in pixels
+                tooltipTitleFontSize: 14,
+
+                // String - Tooltip title font weight style
+                tooltipTitleFontStyle: "bold",
+
+                // String - Tooltip title font colour
+                tooltipTitleFontColor: "#fff",
+
+                // Number - pixel width of padding around tooltip text
+                tooltipYPadding: 6,
+
+                // Number - pixel width of padding around tooltip text
+                tooltipXPadding: 6,
+
+                // Number - Size of the caret on the tooltip
+                tooltipCaretSize: 8,
+
+                // Number - Pixel radius of the tooltip border
+                tooltipCornerRadius: 6,
+
+                // Number - Pixel offset from point x to tooltip edge
+                tooltipXOffset: 10,
+
+                // String - Template string for single tooltips
+                tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+
+                // String - Template string for single tooltips
+                multiTooltipTemplate: "<%= value %>",
+
+                // Function - Will fire on animation progression.
+                onAnimationProgress: function(){},
+
+                // Function - Will fire on animation completion.
+                onAnimationComplete: function(){}
+            };
+
+            // Get context with jQuery - using jQuery's .get() method.
+            var ctx = $("#myChart").get(0).getContext("2d");
+            ctx.canvas.height = getPreferedHeight(1 - config.paddingHeight);
+            ctx.canvas.width = getPreferedWidth(1 - config.paddingWidth);
+            // This will get the first returned node in the jQuery collection.
+            myNewChart = new Chart(ctx).Bar(data, options);
+        },
+        chart: myNewChart,
+        update: function() {
+            myNewChart.update();
+        },
+        getBarsAtEvent: function(event) {
+            // => activeBars is an array of bars on the canvas that are at the same position as the click event.
+            myNewChart.getBarsAtEvent(event);
+        },
+        addData: function(valuesArray, label) {
+            // The values array passed into addData should be one for each dataset in the chart
+            //myBarChart.addData([40, 60], "August");
+            // The new data will now animate at the end of the chart.
+            myNewChart.addData(valuesArray, label);
+        },
+        removeData: function() {
+            // The chart will now animate and remove the first bar
+            myNewChart.removeData();
+        }
+    };
+
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    var options = {
+        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero : true,
+
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
+
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+
+        //Boolean - If there is a stroke on each bar
+        barShowStroke : true,
+
+        //Number - Pixel width of the bar stroke
+        barStrokeWidth : 2,
+
+        //Number - Spacing between each of the X value sets
+        barValueSpacing : 5,
+
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing : 1,
+
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+    };
+
+    return app;
+})(jQuery, Chart);
