70f988baf2969d89347f8abd197bc6602c92145a
Jesús de Mula Cano <jesus.demula@movilizer.com>
2015-02-05 15:29:22 +0100

    add orientation change event listener - properly working

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,72 @@
 var APP = (function($, Chart, window) {
     "use strict";
-    var myNewChart;
+    var myNewChart = {};
     var maxHeight;
     var maxWidth;
 
     var config = {
         paddingHeight: 0.1,
         paddingWidth: 0.1
+    };
+
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+
+    var options = {
+        animation: false,
+
+        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero : true,
+
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines : true,
+
+        //String - Colour of the grid lines
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+
+        //Number - Width of the grid lines
+        scaleGridLineWidth : 1,
+
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+
+        //Boolean - If there is a stroke on each bar
+        barShowStroke : true,
+
+        //Number - Pixel width of the bar stroke
+        barStrokeWidth : 2,
+
+        //Number - Spacing between each of the X value sets
+        barValueSpacing : 5,
+
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing : 1,
+
+        //String - A legend template
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
     };
 
     function setMaxSize(height, width) {
@@ -36,11 +96,11 @@ var APP = (function($, Chart, window) {
             setMaxSize(win.height(), win.width());
 
             //Set container size
-            $("#chart-frame").height(maxHeight).width(maxWidth);
+            //$("#chart-frame").height(maxHeight).width(maxWidth);
 
             //Set graph size
             setCanvasSize(ctx.canvas);
-            myNewChart.update();
+            myNewChart = new Chart(ctx).Bar(data, options);
         });
 
         //Set onResize in case they change to portrait mode
@@ -49,11 +109,11 @@ var APP = (function($, Chart, window) {
             setMaxSize(win.height(), win.width());
 
             //Set container size
-            $("#chart-frame").height(maxHeight).width(maxWidth);
+            //$("#chart-frame").height(maxHeight).width(maxWidth);
 
             //Set graph size
             setCanvasSize(ctx.canvas);
-            myNewChart.update();
+            myNewChart = new Chart(ctx).Bar(data, options);
         });
     }
 
@@ -118,7 +178,7 @@ var APP = (function($, Chart, window) {
                 responsive: false,
 
                 // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-                maintainAspectRatio: true,
+                maintainAspectRatio: false,
 
                 // Boolean - Determines whether to draw tooltips on the canvas or not
                 showTooltips: true,
@@ -195,7 +255,9 @@ var APP = (function($, Chart, window) {
             //Listen to events changes
             connectEvents();
             // This will get the first returned node in the jQuery collection.
+            options.animation = true;
             myNewChart = new Chart(ctx).Bar(data, options);
+            options.animation = false;
         },
         chart: myNewChart,
         update: function() {
@@ -215,64 +277,6 @@ var APP = (function($, Chart, window) {
             // The chart will now animate and remove the first bar
             myNewChart.removeData();
         }
-    };
-
-    var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(220,220,220,0.75)",
-                highlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.5)",
-                strokeColor: "rgba(151,187,205,0.8)",
-                highlightFill: "rgba(151,187,205,0.75)",
-                highlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
-
-    var options = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero : true,
-
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines : true,
-
-        //String - Colour of the grid lines
-        scaleGridLineColor : "rgba(0,0,0,.05)",
-
-        //Number - Width of the grid lines
-        scaleGridLineWidth : 1,
-
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
-
-        //Boolean - If there is a stroke on each bar
-        barShowStroke : true,
-
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth : 2,
-
-        //Number - Spacing between each of the X value sets
-        barValueSpacing : 5,
-
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing : 1,
-
-        //String - A legend template
-        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
     };
 
     return app;
